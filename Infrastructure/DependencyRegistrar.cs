489e7664065e2254f79d888e4fba4ad11dc286bd
Gökhan SEVİM <mail@gokhansevim.com>
2021-12-29 20:26:10 +0300

    Initial commit

---
+++ Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Payments.Iyzico.Controllers;
+using Nop.Plugin.Payments.Iyzico.Services;
+
+namespace Nop.Plugin.Payments.Iyzico.Infrastructure
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        public virtual void Register(IServiceCollection services, ITypeFinder typeFinder, AppSettings appSettings)
+        {
+            services.AddScoped<PaymentIyzicoController>();
+            services.AddScoped<IPaymentIyzicoService, PaymentIyzicoService>();
+        }
+
+        public int Order => 2;
+    }
+}
